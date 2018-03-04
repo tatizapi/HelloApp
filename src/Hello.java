93e452dd8b4b493163e75efc03109f640506d1dd
taty <tatiana.zapirtan@yahoo.com>
2018-03-04 19:50:21 +0200

    Hello file modified

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
 public class Hello
 {
+
     public static void main(String args[])
     {
         System.out.println("Hello");
+    }
+
+    public void f()
+    {
+        System.out.println("whatevar");
     }
 }
